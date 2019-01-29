bc020abb30a45dda4304fbf8866528a3f3ae286c
rexan1 <46243104+rexan1@users.noreply.github.com>
2019-01-29 15:52:16 +0100

    Update 2019-01-07-1a.sh

--- 2019-01-07-1a.sh
+++ 2019-01-07-1a.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 # prompt for username, save as $user
-read -p "Who are you? " user && ((   
-# test if user is logged in
-who -u | grep -q "^$user "   &&  
-# run top with that username
-top -u "$user"                   ) || 
-echo "$user is not logged in"   )     # error message
+read -p "Who are you? " user 
+   
+# test if user is logged in and run top with that username, if not write an error message
+&& (( who -u | grep -q "^$user "  && top -u "$user")  || echo "$user is not logged in"   )   
+
+
+    
 
 
