4a666ca60e97a3fabed2be7c0cd824f083c8cab4
rexan1 <46243104+rexan1@users.noreply.github.com>
2019-01-29 19:12:59 +0100

    Update skript-2019-01-07-2b.sh

--- skript-2019-01-07-2b.sh
+++ skript-2019-01-07-2b.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-
-FILENAME="employee"
-
-sort -k 1 $FILENAME
+#we assign the variabel file to file named employee we created
+file="employee"
+#this will sort the first coloumn in the file 
+sort -k 1 $file
 
 
