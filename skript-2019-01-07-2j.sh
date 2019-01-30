65cd20a72fafa3f31e507ca4ee6273b107273410
rexan1 <46243104+rexan1@users.noreply.github.com>
2019-01-30 21:06:07 +0100

    Update skript-2019-01-07-2j.sh

--- skript-2019-01-07-2j.sh
+++ skript-2019-01-07-2j.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 file="employee"
-#this will show all the records where the condition not equal to E2 is met
+#this will show all the records where the condition not equal to E2 is met.
 awk ' $3 !="E2" ' $file
