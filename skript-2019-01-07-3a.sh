6be39ed1f5f885cff5765b2d42d40656d15e6875
rexan1 <46243104+rexan1@users.noreply.github.com>
2019-01-30 16:07:47 +0100

    Create skript-2019-01-07-3a.sh

---
+++ skript-2019-01-07-3a.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#read -p "What is your favorite color: " color
+
+color="somthing not a color"
+
+#This is a while loop that will run until the condition is met
+
+while [ ! "$color" == "red" ] 
+do 	
+	echo -n "What is your favorite color: "
+	read color
+
+#If condition that prints the right answear if met
+
+		if [ $color == "red" ]		
+		then 
+		echo "That is the right answear"
+			break
+		fi 
+
+#wait 3 seconds before going back to the start of the loop	
+	echo  "Nope wrong answear"
+	sleep 3s
+	
+	
+
+done
