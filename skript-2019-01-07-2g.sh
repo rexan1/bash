b892ab286fc6fb93c65a55175f9baec8ebc170b0
rexan1 <46243104+rexan1@users.noreply.github.com>
2019-01-30 19:38:16 +0100

    Update skript-2019-01-07-2g.sh

--- skript-2019-01-07-2g.sh
+++ skript-2019-01-07-2g.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 file="employee"
-#this will print the second column in the file and will only show lines starting with B
+#this will print the second column in the file and will only show lines starting with B.
 awk '{print $2}' $file | grep -E '\bB'
